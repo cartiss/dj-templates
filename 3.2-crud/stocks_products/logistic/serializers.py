1a2438135e117eca57a4a493db03d7ce67450fec
cartiss <kirill.fesiuk@gmail.com>
2021-12-26 22:44:45 +0200

    Stocks_products without create, update

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,40 @@
 from rest_framework import serializers
 
+from logistic.models import Product, Stock
+
 
 class ProductSerializer(serializers.ModelSerializer):
     # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = '__all__'
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    pass
+    product = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(),
+        required=True,
+    )
+    quantity = serializers.IntegerField(min_value=1, default=1)
+    price = serializers.DecimalField(min_value=1, decimal_places=2, max_digits=18)
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = 'id, address, positions'
+
+    def validate_positions(self, value):
+        if not value:
+            raise serializers.ValidationError("No position")
+
+        product_ids = [item['products'].id for item in value]
+
+        if len(product_ids) != len(set(product_ids)):
+            raise serializers.ValidationError('Дублируются позиции на складе')
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
